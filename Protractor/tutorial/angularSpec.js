3d5eabfa1fc45d445071a09df30f8eaff74c25ec
Pedro <pedro.souza.psj@gmail.com>
2018-03-19 22:58:15 -0400

    Tirando uma screenshot durante o teste

--- Protractor/tutorial/angularSpec.js
+++ Protractor/tutorial/angularSpec.js
@@ -1,9 +1,25 @@
 var angularHomepage = require('./AngularHomepage');
+var fs = require('fs');
+
+// abstract writing screen shot to a file
+function writeScreenShot(data, filename) {
+    var stream = fs.createWriteStream(filename);
+
+    stream.write(new Buffer(data, 'base64'));
+    stream.end();
+}
+
 describe('angularjs homepage test',function() {
 
   it('should greet the named user',function() {
     angularHomepage.get();
     angularHomepage.setName('Pedro Souza Junior');
     expect(angularHomepage.getGreeting()).toEqual('Hello Pedro Souza Junior!');
+  });
+
+  it('should take a snapshot',function() {
+    browser.takeScreenshot().then(function (png) {
+      writeScreenShot(png, 'exception.png');
+    });
   });
 });
